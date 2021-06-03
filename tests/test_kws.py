94c873cca5abc5d034aed2414e3cc1811015e688
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-06-03 18:04:24 +0700

    update test

---
+++ tests/test_kws.py
@@ -0,0 +1,42 @@
+import os
+from blossom import learners
+
+from blossom.models import MHAttKWS
+from blossom.datasets import MHAttDataset
+from blossom.learners import MHAttKWSLearner
+
+def test_train():
+
+    train_dataset = MHAttDataset(
+        mode='train',
+        root='./data/v2.12'
+    )
+    test_dataset = MHAttDataset(
+        mode='test',
+        root='./data/v2.12'
+    )
+
+    model = MHAttKWS(
+        num_classes=2,
+        in_channel=1,
+        hidden_dim=128,
+        n_head=4,
+        dropout=0.1
+    )
+
+    learner = MHAttKWSLearner(model=model)
+    learner.train(
+        train_dataset=train_dataset,
+        test_dataset=test_dataset,
+        batch_size=48,
+        learning_rate=1e-4,
+        eps=1e-8,
+        betas=(0.9, 0.999),
+        max_steps=5,
+        n_epochs=2,
+        shuffle=True,
+        num_workers=8,
+        view_model=True,
+        save_path='./models',
+        model_name='mhatt_model'
+    )
